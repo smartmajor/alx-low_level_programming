a00bb04ad33a9c7fb947cc8d78dd73e21047fcbd
smartmajor <dan333ngidi@yahoo.com>
2023-03-21 13:56:45 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - program prints _putchar, followed by a new line
@@ -16,5 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	Return(0);
+	
+	return(0);
 }

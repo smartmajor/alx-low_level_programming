5425dd95de6cab946dccbea96ecdc7e5372596a4
smartmajor <dan333ngidi@yahoo.com>
2023-03-22 13:09:31 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - function checks for absolute value
+ * @a: parameter to be checked
+ * Return: 0 (Success)
+ */
+
+
+int _abs(int a)
+{
+	if (a < 0)
+		a = -(a);
+	else if (a >= 0)
+		a = (a);
+	return (a);
+}

5c1f5f25c4d6ece013ac08ccd239d83134923804
smartmajor <dan333ngidi@yahoo.com>
2023-03-27 04:55:03 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -7,13 +7,13 @@
  * Return: Always 0.
  */
 
-int main(void);
+int main(void)
 {
 	int n;
 
 	n = 402;
 	printf("n=%d\n", n);
-	reset_to_98(&n)
+	reset_to_98(&n);
 		printf("n=%d\n", n);
 	return (0);
 }

c9a881ab5433d6e040efe94085f04224f269d93d
smartmajor <dan333ngidi@yahoo.com>
2023-04-07 17:50:04 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - function
+ * @argc: counts arguments
+ * @argv: list the array of strings
+ * Return: Always 0 (Success)
+ */
+
+int main(int __attribute__((unused))argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

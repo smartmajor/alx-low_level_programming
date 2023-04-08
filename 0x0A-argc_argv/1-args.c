4ef45047fa6e4b183cef76b899b5261f07e503d0
smartmajor <dan333ngidi@yahoo.com>
2023-04-07 18:20:43 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - function
+ * @argc: counts arguments
+ * @argv: counts strings in arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}

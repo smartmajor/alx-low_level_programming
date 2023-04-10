6647dde0385e86ec538c3fbd9250faf6fcf78bc1
smartmajor <dan333ngidi@yahoo.com>
2023-04-09 17:09:40 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -36,3 +36,30 @@ int main(int argc, char *argv[])
 	printf("%d\n", sum);
 	return (0);
 }
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * check_num - function checks string for digit
+ * @str: str array
+ * Return: Always 0 (Success)
+ */
+
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}

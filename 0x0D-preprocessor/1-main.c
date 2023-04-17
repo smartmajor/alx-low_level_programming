8f124b153eaefb6c0e9dfd80eedabd94dcfed8f8
smartmajor <dan333ngidi@yahoo.com>
2023-04-17 05:55:26 -0700

    1-pi.h

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,22 @@
+#include "1-pi.h"
+#include "1-pi.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	float r;
+	float n;
+
+	r = 98;
+	n = PI * r * r;
+
+	printf("%.3f\n", n);
+
+	return (0);
+}
